d45f661f9b099b4dc08ee720b0486394bf585efd
Dominik <ledom.git@gmail.com>
2016-08-10 17:30:05 +0200

    Add Navbar component (with Store and Actions)

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+               <Navbar />
                 {this.props.children}
                 <Footer />
             </div>
