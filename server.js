e21127a00820fdbdfd3340dce224b37bdf231040
Dominik <ledom.git@gmail.com>
2016-08-11 09:59:10 +0200

    Socket.IO - Real-time User Count

--- server.js
+++ server.js
@@ -17,28 +17,57 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware
-app.use(function(req, res) {
+app.use(function (req, res) {
     // Note that req.url here should be the full URL path from
     // the original request, including the query string.
-    Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
-       if (err) {
-           res.status(500).send(err.message);
-       } else if (redirectLocation) {
-           res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
-       } else if (renderProps) {
-           var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
-           var page = swig.renderFile('views/index.html', { html: html });
-           res.status(200).send(page);
-       } else {
-           res.status(404).send('Page Not Found');
-       }
+    Router.match({
+        routes: routes.default,
+        location: req.url
+    }, function (err, redirectLocation, renderProps) {
+        if (err) {
+            res.status(500).send(err.message);
+        } else if (redirectLocation) {
+            res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
+        } else if (renderProps) {
+            var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+            var page = swig.renderFile('views/index.html', {
+                html: html
+            });
+            res.status(200).send(page);
+        } else {
+            res.status(404).send('Page Not Found');
+        }
     });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function (socket) {
+    onlineUsers++;
+
+    io.sockets.emit('onlineUsers', {
+        onlineUsers: onlineUsers
+    });
+
+    socket.on('disconnect', function () {
+        onlineUsers--;
+        io.sockets.emit('onlineUsers', {
+            onlineUsers: onlineUsers
+        });
+    });
+});
+
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
