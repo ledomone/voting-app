cf684380a02a68a0db1e56de36c5238e3ab8d4c0
Dominik <ledom.git@gmail.com>
2016-08-12 15:42:05 +0200

    Add API routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var routes = require('./app/routes');
 var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
+var _ = require('underscore');
 
 var express = require('express');
 var path = require('path');
@@ -114,6 +115,173 @@ app.post('/api/characters', function (req, res, next) {
             });
     }
   ]);
+});
+
+/** 
+ * GET /api/characters
+ * Returns 2 random characters of the same gender that have not been voted yet.
+ */
+app.get('/api/characters', function(req, res, next) {
+    var choices = ['Female', 'Male'];
+    var randomGender = _.sample(choices);
+    
+    Character.find({ random: { $near: [Math.random(), 0] } })
+    .where('voted', false)
+    .where('gender', randomGender)
+    .limit(2)
+    .exec(function(err, characters) {
+        if (err) return next(err);
+        
+        if (characters.length === 2) {
+            return res.send(characters);
+        }
+        
+        var oppositeGender = _.first(_.without(choices, randomGender));
+        
+        Character
+        .find({ random: { $near: [Math.random(), 0] } })
+        .where('voted', false)
+        .where('gender', oppositeGender)
+        .limit(2)
+        .exec(function(err, characters) {
+            if (err) return next(err);
+            
+            if (characters.length === 2) {
+                return res.send(characters);
+            }
+            
+            Character.update({}, { $set: { voted: false } }, { multi: true }, function (err) {
+                if (err) return next(err);
+                res.send([]);
+            });
+        });
+    });
+});
+
+/**
+ * PUT /api/characters
+ * Update winning and losing count for both characters.
+ */
+
+app.put('/api/characters', function(req, res, next) {
+    var winner = req.body.winner;
+    var loser = req.body.loser;
+    
+    if (!winner || !loser) {
+        return res.status(400).send({ message: 'Voting requires two characters.' });
+    }
+    
+    if (winner === loser) {
+        return res.status(400).send({ message: 'Cannot vote for and against the same character.'});
+    }
+    
+    async.parallel([
+        function(callback) {
+            Character.findOne({ characterId: winner }, function(err, winner) {
+                callback(err, winner);
+            });
+        },
+        function(callback) {
+            Character.findOne({ characterId: loser }, function(err, loser) {
+                callback(err, loser);
+            });
+        }
+    ],
+    function(err, results) {
+        if (err) return next(err);
+        
+        var winner = results[0];
+        var loser = results[1];
+        
+        if (!winner || !loser) {
+            return res.status(404).send({ message: 'One of the characters no longer exists.' });
+        }
+        
+        if (winner.voted || loser.voted) {
+            res.status(200).end();
+        }
+        
+        async.parallel([
+            function(callback) {
+                winner.wins++;
+                winner.voted = true;
+                winner.random = [Math.random(), 0];
+                winner.save(function(err) {
+                    callback(err);
+                });
+            },
+            function(callback) {
+                loser.losses++;
+                loser.voted = true;
+                loser.random = [Math.random(), 0];
+                loser.save(function(err) {
+                    callback(err);
+                });
+            }
+        ], function(err) {
+            if (err) return next(err);
+            res.status(200).end();
+        });
+    });
+});
+
+/** 
+ * GET /api/characters/count
+ * Returns the total number of characters.
+ */
+app.get('/api/characters/count', function(req, res, next) {
+    Character.count({}, function(err, count) {
+        if (err) return next(err);
+        res.send({ count: count });
+    })
+});
+
+/** 
+ * GET /api/characters/search
+ * Looks up a character by name. (case-insensitive)
+ */
+app.get('/api/characters/search', function(req, res, next) {
+    var characterName = new RegExp(req.query.name, 'i');
+    
+    Character.findOne({ name: characterName }, function(err, character) {
+        if (err) return next(err);
+        
+        if (!character) {
+            return res.status(404).send({ message: 'Character not found.' });
+        }
+        
+        res.send(character);
+    });
+});
+
+/** 
+ * GET /api/characters/top
+ * Return 100 highest ranked characters. Filter by gender, race and bloodline.
+ */
+app.get('/api/characters/top', function(req, res, next) {
+    var params = req.query;
+    var conditions = {};
+    
+    _.each(params, function(value, key) {
+        conditions[key] = new RegExp('^' + value + '$', 'i');
+    });
+    
+    Character
+    .find(conditions)
+    .sort('-wins') // Sort in descending order (highest wins on top)
+    .limit(100)
+    .exec(function(err, characters) {
+        if (err) return next(err);
+        
+        // Sort by winning percentage
+        characters.sort(function(a, b) {
+            if (a.wins / (a.wins + a.losses) < b.wins / (b.wins + b.losses)) { return 1; }
+            if  (a.wins / (a.wins + a.losses) > b.wins / (b.wins + b.losses)) { return -1; }
+            return 0;
+        });
+        
+        res.send(characters);
+    });
 });
 
 // middleware
